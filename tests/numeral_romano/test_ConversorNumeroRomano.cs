54c18fd79e68920c60d01067a2a3a1850680cfac
Giuliano Beltrami <juka2027@live.com>
2022-02-21 17:24:45 -0300

    Cap 3

--- tests/numeral_romano/test_ConversorNumeroRomano.cs
+++ tests/numeral_romano/test_ConversorNumeroRomano.cs
@@ -3,14 +3,14 @@ using Xunit;
 
 namespace tests
 {
-    public class TestConversor
+    public class TestConversorDeNumeroRomano
     {
         [Fact]
         public void deveEntenderOSimboloI()
         {
-            var romano = new ConversorDeNumeroRomano();
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
 
-            var simboloConvertido = romano.converter("I");
+            int simboloConvertido = romano.Converter("I");
 
             Assert.Equal(1, simboloConvertido);
         }
@@ -18,11 +18,50 @@ namespace tests
         [Fact]
         public void deveEntenderOSimboloV()
         {
-            var romano = new ConversorDeNumeroRomano();
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
 
-            var simboloConvertido = romano.converter("V");
+            int simboloConvertido = romano.Converter("V");
 
             Assert.Equal(5, simboloConvertido);
+        }
+
+        [Fact]
+        public void deveEntenderOSimboloII()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+
+            int simboloConvertido = romano.Converter("II");
+
+            Assert.Equal(2, simboloConvertido);
+        }
+
+        [Fact]
+        public void deveEntenderOSimboloXXII()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+
+            int simboloConvertido = romano.Converter("XXII");
+
+            Assert.Equal(22, simboloConvertido);
+        }
+
+        [Fact]
+        public void deveEntenderOSimboloIX()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+
+            int simboloConvertido = romano.Converter("IX");
+
+            Assert.Equal(9, simboloConvertido);
+        }
+
+        [Fact]
+        public void deveEntenderOSimboloXXIV(){
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+
+            int simboloConvertido = romano.Converter("XXIV");
+
+            Assert.Equal(24, simboloConvertido);
         }
     }
 }
